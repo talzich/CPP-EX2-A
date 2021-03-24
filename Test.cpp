ad97c1d84347cc16b3a5d70e76a726021b2ab406
Tal <talzhtc@gmail.com>
2021-03-24 16:40:56 +0200

    Changed in doc

--- Test.cpp
+++ Test.cpp
@@ -13,12 +13,11 @@
 
 using namespace std;
 using namespace ariel;
+using ariel::Direction;
 
-const int num_of_tests = 5*6 + 4*2; // = 38 tests.
+const int NUM_OF_TESTS = 5*6 + 4*2; // = 38 tests.
 
-// Assuming messageboard's frame is finite 
-// and consists of 10 rows and 10 columns.
-const unsigned int ROWS = 10, COLS = 10;
+const unsigned int ROWS = 100, COLS = 100;
 const vector<string> INPUTS {"Tal", "Zichlinsky", "Horizontal", "Vertical"};
 
 
@@ -28,7 +27,7 @@ TEST_CASE("Test read() on an empty Board:") {
 
     Board empty_board;
 
-    Direction direction = Direction::Horizontal;
+    Direction dir = Direction::Horizontal;
 
     unsigned int random_length;
 
@@ -36,35 +35,34 @@ TEST_CASE("Test read() on an empty Board:") {
         for(unsigned int j = 0; j < COLS/2; j++) {
 
             // Switch direction every iteration
-            if(direction == Direction::Horizontal) { 
+            if(dir == Direction::Horizontal) { 
 
                 // Reading vertically. 
-                direction = Direction::Vertical;  
+                dir = Direction::Vertical;  
                 random_length = ((uint)rand()%((uint)(ROWS-i))+1);
             } else {
                 // Reading Horizontally.
-                direction = Direction::Horizontal;
+                dir = Direction::Horizontal;
                 random_length = ((uint)rand()%((uint)(COLS-j))+1);
             }
 
             cout << i << ", " << j << endl;
             cout << "rand length = " << random_length << endl;
             string expected_out(random_length, '_');
-            CHECK(empty_board.read(i, j, direction, random_length) == expected_out);            
+            CHECK(empty_board.read(i, j, dir, random_length) == expected_out);            
         }
     }
 }
 
-TEST_CASE("Test post() as new posts should always be displayed first:") {
+TEST_CASE("Test post") {
 
-    // Reset random vars.
-    srand((unsigned)time(NULL));
+    
 
     Board board;
     Direction direction = Direction::Horizontal;
+    srand((unsigned)time(NULL));
 
-
-    // For loop checks if last posted message is returned from read().
+    // Testing if message that was posted last is the one to return 
     for(uint i = 0; i < INPUTS.size(); i++) {
         direction = i%2 == 0 ? Direction::Horizontal : Direction::Vertical;
         CHECK_NOTHROW(board.post(i, 0, direction, INPUTS.at(i)));
@@ -72,19 +70,7 @@ TEST_CASE("Test post() as new posts should always be displayed first:") {
         CHECK(INPUTS.at(i) == board.read(i, 0, direction, INPUTS.at(i).size()));
     }
 
-    // This section checks if a new post overrides a previous post.
-
-    /*  This test's outcome:
-        read(0,0,horizontal, 4) == 'aaaa'
-        and then
-        read(0,0,horizontal, 4) == 'aaab'
-        aaaa_       aaab_
-        _____       ___b_
-        _____   ->  ___b_
-        _____   ->  ___b_
-        _____       _____
-    */
-    string message = "aaaa";
+    string message = "zich";
     CHECK_NOTHROW(board.post(0,0, direction, message));
     CHECK(board.read(0,0, direction, message.size()) == message);
 
@@ -95,26 +81,3 @@ TEST_CASE("Test post() as new posts should always be displayed first:") {
     CHECK(board.read(0,0, direction, message.size()) != message);
     CHECK(board.read(0,0, direction, message.size()) == "aaab");
 }
-
-
-// TEST_CASE
-
-// TEST_CASE("Test post() as new posts should override previous posts:") {
-
-//     srand((unsigned)time(NULL));
-
-//     Board board;
-//     Direction direction = Direction::Horizontal;
-
-//     string message = "aaaa";
-
-//     board.post(0,0, direction, message);
-//     CHECK(board.read(0,0, direction, message.size()) == message);
-
-//     direction = Direction::Vertical;
-//     board.post(0, message.size()-1, direction, "bbbb");
-
-//     direction = Direction::Horizontal;
-//     CHECK(board.read(0,0, direction, message.size()) != message);
-//     CHECK(board.read(0,0, direction, message.size()) == "aaab");
-// }
